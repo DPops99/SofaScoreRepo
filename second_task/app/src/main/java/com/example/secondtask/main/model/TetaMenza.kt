d0b31df8e018fd982c1199400e2dca810529cb18
DanijelPopovic <Danijel.Popovic@fer.hr>
2021-03-21 20:07:53 +0100

    third_task completed

--- second_task/app/src/main/java/com/example/secondtask/main/model/TetaMenza.kt
+++ second_task/app/src/main/java/com/example/secondtask/main/model/TetaMenza.kt
@@ -1,16 +1,24 @@
 package com.example.secondtask.main.model
 
+import com.example.secondtask.main.model.enums.Job_Position
+import com.example.secondtask.main.model.enums.Workplace
 import java.io.Serializable
 
 class TetaMenza constructor(
-    var first_name :String,
-    var last_name : String,
-    var workplace : String,
-    var age : Int,
-    var OIB : Int
+        var first_name :String,
+        var last_name : String,
+        var workplace : Workplace,
+        var age : Int,
+        var OIB : Int,
+        var gender : String,
+        var job_position : Job_Position,
+        var job_shift : String,
+        var email : String,
+        var address : String
 ):Serializable{
 
     override fun toString(): String {
-        return first_name+" "+last_name+" "+workplace+" "+ age.toString()+" "+OIB.toString()
+        return first_name+" "+last_name+" "+workplace.toString()+" "+ age.toString()+" "+OIB.toString()+" "+gender+
+                " "+job_position.toString()+" "+job_shift+" "+email+" "+address
     }
 }
---
+++ second_task/app/src/main/java/com/example/secondtask/main/model/enums/Workplace.kt
@@ -0,0 +1,5 @@
+package com.example.secondtask.main.model.enums
+
+enum class Workplace {
+    SD_STJEPAN_RADIC, SD_CVJETNO_NASELJE, CASSANDRA, FFZG
+}
